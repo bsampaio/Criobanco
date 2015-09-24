864242979f6a23c46a9285a22645b9acaabda964
Breno Grillo <bsampaio@outlook.com>
2015-09-24 15:48:26 -0300

    Acessando BD heroku

--- config/database.php
+++ config/database.php
@@ -11,11 +11,12 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule();
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'criativa',
-    'username'  => 'root',
-    'password'  => 'root',
+    'driver'    => 'psql',
+    'host'      => 'ec2-54-83-205-164.compute-1.amazonaws.com',
+    'database'  => 'd6ankflvuh8jcl',
+    'username'  => 'hymfeihwiisdcr',
+    'password'  => 'Wp1VcdjpPjTzfVVX_JN1LoPeEl',
+    'port'      => '5432',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
