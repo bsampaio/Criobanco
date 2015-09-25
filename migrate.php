c32a6e17bb47c5a4b07c45769f6b9fe1917d7f67
Breno Grillo <bsampaio@outlook.com>
2015-09-24 22:39:01 -0300

    Modificando driver pgsql

--- migrate.php
+++ migrate.php
@@ -10,30 +10,32 @@
 require_once 'vendor/autoload.php';
 require_once 'config/database.php';
 
-exec("php --version > version.txt");
-
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
 
-Capsule::schema()->create('criobanco_evento', function(Blueprint $table){
+if(!Capsule::schema()->hasTable('criobanco_evento')){
+    Capsule::schema()->create('criobanco_evento', function(Blueprint $table){
 
-    $table->increments('id');
-    $table->timestamps();
-    $table->string('horario');
-    $table->integer('vagas')->unsigned();
-});
+        $table->increments('id');
+        $table->timestamps();
+        $table->string('horario');
+        $table->integer('vagas')->unsigned();
+    });
+}
 
-Capsule::schema()->create('criobanco_inscricao', function(Blueprint $table){
+if(!Capsule::schema()->hasTable('criobanco_evento')){
+    Capsule::schema()->create('criobanco_inscricao', function(Blueprint $table){
 
-    $table->increments('id');
-    $table->timestamps();
-    $table->integer('evento_id')->unsigned();
-    $table->foreign('evento_id')->references('id')->on('criobanco_evento');
-    $table->string('email_pai',150);
-    $table->string('name_pai', 200);
-    $table->string('telefone_pai', 30);
-    $table->string('email_mae',150);
-    $table->string('name_mae', 200);
-    $table->string('telefone_mae', 30);
-    $table->integer('filhos')->unsigned();
-});
+        $table->increments('id');
+        $table->timestamps();
+        $table->integer('evento_id')->unsigned();
+        $table->foreign('evento_id')->references('id')->on('criobanco_evento');
+        $table->string('email_pai',150);
+        $table->string('name_pai', 200);
+        $table->string('telefone_pai', 30);
+        $table->string('email_mae',150);
+        $table->string('name_mae', 200);
+        $table->string('telefone_mae', 30);
+        $table->integer('filhos')->unsigned();
+    });
+}
