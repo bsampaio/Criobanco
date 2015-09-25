17e02b720f1e747dcf7ef5aab3c905630ac967d8
Breno Grillo <bsampaio@outlook.com>
2015-09-24 22:51:48 -0300

    Creating RESTful API to list events

---
+++ list-events.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once 'vendor/autoload.php';
+require_once 'config/database.php';
+
+use App\Models\Event;
+
+echo json_encode(Event::all()->toArray());
